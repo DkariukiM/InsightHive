c226f444ee1e5aa36f359765724bc5df48b392bd
David Kariuki <58103133+DkariukiM@users.noreply.github.com>
2023-04-28 12:38:30 +0300

    Dashboard layout completion

--- InsightHive/react/src/components/DefaultLayout.jsx
+++ InsightHive/react/src/components/DefaultLayout.jsx
@@ -14,7 +14,7 @@ const navigation = [
   { name: 'Surveys', to: '/surveys'},
 ]
 const userNavigation = [
-  { name: 'Settings', href: '#' },
+  { name: 'Signout', href: '#' },
 ]
 
 function classNames(...classes) {
@@ -22,6 +22,12 @@ function classNames(...classes) {
 }
 
 export default function DefaultLayout() {
+
+  const logout = (ev) => {
+    ev.preventDefault();
+    console.log("Logout");
+  }
+
   return (
     <>
      
@@ -60,13 +66,7 @@ export default function DefaultLayout() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      <button
-                        type="button"
-                        className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
+                      
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
@@ -86,21 +86,15 @@ export default function DefaultLayout() {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
+                              <Menu.Item>
                                   <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
+                                    href="#"
+                                    onClick = {(ev) => logout(ev)}
+                                    className={'block px-4 py-2 text-sm text-gray-700'}
                                   >
-                                    {item.name}
+                                    Sign Out
                                   </a>
-                                )}
                               </Menu.Item>
-                            ))}
                           </Menu.Items>
                         </Transition>
                       </Menu>
@@ -123,18 +117,16 @@ export default function DefaultLayout() {
               <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navigation.map((item) => (
-                    <Disclosure.Button
+                    <NavLink
                       key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      to={item.to}
+                      className={({ isActive }) => classNames(
+                        isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                         'block rounded-md px-3 py-2 text-base font-medium'
                       )}
-                      aria-current={item.current ? 'page' : undefined}
                     >
                       {item.name}
-                    </Disclosure.Button>
+                    </NavLink>
                   ))}
                 </div>
                 <div className="border-t border-gray-700 pb-3 pt-4">
@@ -146,23 +138,17 @@ export default function DefaultLayout() {
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
                       <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                     </div>
-                    <button
-                      type="button"
-                      className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
+                    
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
                       <Disclosure.Button
-                        key={item.name}
                         as="a"
-                        href={item.href}
+                        href="#"
+                        onClick={(ev) => logout(ev)}
                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                       >
-                        {item.name}
+                        Sign Out
                       </Disclosure.Button>
                     ))}
                   </div>
